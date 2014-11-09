2aabebb8ea79727a5578717c92b622a24a5ae111
Vir Patel <virpatel3@gmail.com>
2014-11-09 11:30:12 -0600

    make it better

--- mysql2json.py
+++ mysql2json.py
@@ -1,6 +1,10 @@
-import _mysql
+import _mysql, json
 db=_mysql.connect("localhost","virpatel","password","laundry_app")
 db.query('SELECT * from machines')
 r = db.store_result()
-print r.fetch_row()[0]
-print 'test'
+print json.dumps(r.fetch_row()[0])
+print 'hello world'
+
+
+for i in range(100):
+	print 'vir is a fuck'
